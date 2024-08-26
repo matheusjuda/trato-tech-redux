ffa1a9bff1a30a8a8e9c5a0d187fec1ed4577330
matheusjuda <matheusbj582@gmail.com>
2024-08-26 15:56:14 -0300

    Header e footer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Navbar from './components/Navbar';
+import Router from 'routes';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <Navbar />
+      <Router />
     </div>
   </React.StrictMode>
 );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Header from 'components/Header';
+import styles from './Home.module.scss';
+import relogio from 'assets/inicial.png'
+export default function Home() {
+  return (
+    <div>
+      <Header
+        titulo='Classificados Tech'
+        descricao='Compre diversos produtos no melhor site do Brasil!'
+        imagem={relogio}
+        className={styles.header}
+      />
+    </div>
+  )
+}
