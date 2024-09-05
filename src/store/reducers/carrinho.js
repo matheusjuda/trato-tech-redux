2d49ca5ddee6b5fe62091bdbd72a246310c054c6
matheusjuda <matheusbj582@gmail.com>
2024-09-05 14:43:14 -0300

    Modificando card do produto se estiver dentro da page carrinho

--- src/store/reducers/carrinho.js
+++ src/store/reducers/carrinho.js
@@ -16,12 +16,18 @@ const carrinhoSlice = createSlice({
         }
       ]
       return state.filter(item => item.id !== payload);
+    },
+    mudarQuantidade: (state, { payload }) => {
+      state = state.map(itemNoCarrinho => {
+        if (itemNoCarrinho.id === payload.id) itemNoCarrinho.quantidade += payload.quantidade;
+        return itemNoCarrinho;
+      })
     }
   }
 });
 
 
-export const { mudarCarrinho } = carrinhoSlice.actions;
+export const { mudarCarrinho, mudarQuantidade } = carrinhoSlice.actions;
 
 export default carrinhoSlice.reducer;
 //Se eu quiser mudar o estado (remover ou adicionar carrinho) = Immer (Imutabilidade )
